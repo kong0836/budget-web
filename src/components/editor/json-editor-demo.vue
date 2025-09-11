383f17f3969f5304aeb5c2f1059f60d24eedc855
konglinghui <17610760836@163.com>
2025-09-11 22:43:44 +0800

    feat: 表单校验

--- src/components/editor/json-editor-demo.vue
+++ src/components/editor/json-editor-demo.vue
@@ -1,10 +1,18 @@
 <template>
-    <json-editor v-model="value" />
+    <div>
+    <el-form :model="form" ref="formRef" :rules="formRules" :label-width="120">
+        <el-form-item label="数据" prop="value">
+            <json-editor ref="jsonEditorRef" v-model="form.value" />
+        </el-form-item>
+    </el-form>
+    <el-button type="primary" @click="submitForm">提交</el-button>
+    </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import JsonEditor from '@/components/editor/json-editor.vue';
+import { Form } from 'element-ui';
 
 @Component({
     name: 'JsonEditorDemo',
@@ -13,6 +21,20 @@ import JsonEditor from '@/components/editor/json-editor.vue';
     },
 })
 export default class JsonEditorDemo extends Vue {
-    value = '{"a": "b"}';
+    form = {
+        value : '{"a": "b"}',
+    };
+    valueValidator(rule: any, value: any,callback: any){
+        const valid = (this.$refs.jsonEditorRef as JsonEditor).handleValidate();
+        callback(valid ? undefined: '请输入正确的json格式');
+    }
+    formRules = {
+        value: [
+            { required: true, validator: this.valueValidator, trigger: 'change' },
+        ],
+    };
+    async submitForm(){
+        await (this.$refs.formRef as Form).validate();
+    }
 }
 </script>
