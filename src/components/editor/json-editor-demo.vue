d263377e5914e1ecd766b6187ac0be2970acd0d1
konglinghui <17610760836@163.com>
2025-09-13 09:29:27 +0800

    feat: 修复eslint问题

--- src/components/editor/json-editor-demo.vue
+++ src/components/editor/json-editor-demo.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <el-form
-            :model="form"
             ref="formRef"
+            :model="form"
             :rules="formRules"
             :label-width="120">
             <el-form-item label="数据" prop="value">
@@ -12,12 +12,10 @@
         <el-button type="primary" @click="submitForm">提交</el-button>
     </div>
 </template>
-
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
 import JsonEditor from '@/components/editor/json-editor.vue';
 import { Form } from 'element-ui';
-
+import { Component, Vue } from 'vue-property-decorator';
 @Component({
     name: 'JsonEditorDemo',
     components: {
@@ -28,15 +26,18 @@ export default class JsonEditorDemo extends Vue {
     form = {
         value : '{"a": "b"}',
     };
+
     valueValidator(rule: any, value: any, callback: any){
         const valid = (this.$refs.jsonEditorRef as JsonEditor).handleValidate();
         callback(valid ? undefined : '请输入正确的json格式');
     }
+
     formRules = {
         value: [
             { required: true, validator: this.valueValidator, trigger: 'change' },
         ],
     };
+
     async submitForm(){
         await (this.$refs.formRef as Form).validate();
     }
